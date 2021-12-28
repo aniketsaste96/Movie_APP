0d53fcdcd15e0986481557b134e3cb79a981e7f4
Aniket <aniketsaste96@gmail.com>
2021-12-28 22:45:37 +0530

    redux

--- src/UpdateMovie.js
+++ src/UpdateMovie.js
@@ -1,153 +1,218 @@
-import { useParams } from "react-router-dom";
+
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Button from '@mui/material/Button';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import { useState, useEffect } from "react";
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Paper, label } from '@mui/material';
+import Button from '@mui/material/Button';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from 'yup';
 
-
-export function UpdateMovie() {
+export function EditMovie() {
     const history = useHistory();
-    const [editMovie, setEdit] = useState([]);
-    console.log(editMovie);
-    const [name, setName] = useState("");
-    const [poster, setPoster] = useState("");
-    const [rating, setRating] = useState("");
-    const [summary, setSummary] = useState("");
-    const [trailer, setTrailer] = useState("");
+    const [movie, setMovie] = useState(null);
 
     const { id } = useParams();//to get the id
     //how to get the particular movie when clicked 
 
     //fetch movie by id
-    const UpdateMovie = () => {
-        const UpMovie = {
-            name: name,
-            poster: poster,
-            rating: rating,
-            summary: summary,
-            trailer: trailer
-        };
-
-
-        //1.Post
-        // 2.Body ->data and json data.
-        //3.headers ->you have to say we are passibg json data
-        fetch(`https://61c412cff1af4a0017d9927b.mockapi.io/movies/${id}`, {
-            method: "PUT",
-            body: JSON.stringify(UpMovie
-                //     {
-
-                //     name: editMovie.name,
-                //     poster: editMovie.poster,
-                //     rating: editMovie.rating,
-                //     summary: editMovie.summary,
-                //     trailer: editMovie.trailer
-
-                // }
-            ),
-            headers: {
-                Accept: "application/json",
-                "Content-type": "application/json"
-            }
-        })
-            .then((data) => data.json())
-            .then(() => history.push("../Mymovies"));
-
-
-    }
-
-
     let getMovie = () => {
         fetch(`https://61c412cff1af4a0017d9927b.mockapi.io/movies/${id}`, {
             method: "GET"
         }).then((data) => data.json())
-            .then((mv) => setEdit(mv));
+            .then((mv) => setMovie(mv));
+    }
+    useEffect(getMovie, []);
+
+    //Logic to avoid race condition
+    return movie ? <UpdateMovie movie={movie} /> : "";
+}
+
+
+//race is between update movie (form) and GET movie details
+//Update movie form is winning
+function UpdateMovie({ movie }) {
+    console.log("Update", movie, movie.name)
+
+    // const [name, setName] = useState(movie.name);
+    // const [poster, setPoster] = useState(movie.poster);
+    // const [rating, setRating] = useState(movie.rating);
+    // const [summary, setSummary] = useState(movie.summary);
+    // const [trailer, setTrailer] = useState(movie.trailer);
+
+
+    //useHistory to change the URL
+    const history = useHistory();
+
+
+
+
+
+    // const [user, setUser] = useState(InitialValues);
+    // const [name, poster, ratings, summary] = user;
+
+
+
+
+
+    const validationSchema = yup.object({
+        name: yup.string().required("Please Enter name"),
+        poster: yup.string().url().required("Please provide poster url"),
+        rating: yup.number().required("Please give rating"),
+        summary: yup.string().required("Please type summary"),
+        trailer: yup.string().url().required("please provide trailer URL")
+
+    })
+    const { values, handleSubmit, handleChange, handleBlur, touched, errors } = useFormik({
+        initialValues: {
+            name: "",
+            poster: "",
+            rating: "",
+            summary: "",
+            trailer: ""
+
+        },
+        // validationSchema: formvalidateSchema,
+        onSubmit: (EditMovie) => {
+            console.log(JSON.stringify(EditMovie))
+            editMovie(EditMovie)
+        },
+        validationSchema: validationSchema
+
+    })
+
+    const editMovie = (EditMovie) => {
+        // const UpdatedMovie = {
+        //     name: name,
+        //     poster: poster,
+        //     rating: rating,
+        //     summary: summary,
+        //     trailer: trailer
+        // };
+
+
+        //1.PUT
+        // 2.Body ->data and json data.
+        //3.headers ->you have to say we are passibg json data
+        fetch(`https://61c412cff1af4a0017d9927b.mockapi.io/movies/${movie.id}`, {
+            method: "PUT",
+            body: JSON.stringify(EditMovie),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+            .then((data) => data.json())
+            .then(() => history.push('./Mymovies'));
+
+
     }
 
 
-    useEffect(getMovie, []);
-    // var movie = movies[id];
-    const handleChange0 = (event) => setName(event.target.value);
-    const handleChange1 = (event) => setPoster(event.target.value);
-    const handleChange2 = (event) => setRating(event.target.value);
-    const handleChange3 = (event) => setSummary(event.target.value);
-    const handleChange4 = (event) => setTrailer(event.target.value);
+
+
+
+
 
     return (
 
-        //Get the value from URL
-        //here use new hook called useParams-get the value from URL and to give it ot componant.
         <Box sx={{ '& > :not(style)': { m: 1 } }}>
-            <>
-                {<div className="add-movie-form">
+            {<form className="add-movie-form" onSubmit={handleSubmit}>
+                <TextField
+                    id="name"
+                    name="name"
+                    className="inputfield"
+                    fullWidth label="Enter Movie Name" type="text"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.name && errors.name ? errors.name : ""}
+                    helperText={touched.name && errors.name ? errors.name : ""}
 
-                    <TextField
-                        key={editMovie.name}
-                        className="inputfield"
-                        fullWidth type="text"
-                        defaultValue={editMovie.name}
-                        name="name"
-                        // value={editMovie.name}
-                        onChange={handleChange0}
-                        variant="filled" />
+                    variant="filled" />
+                {touched.name && errors.name ? errors.name : ""}
 
-                    <TextField
-                        key={editMovie.poster}
-                        className="inputfield"
-                        variant="filled"
-                        fullWidth type="text"
-                        defaultValue={editMovie.poster}
-                        name="poster"
-                        // value={editMovie.poster}
+                <TextField
+                    id="poster"
+                    name="poster"
+                    className="inputfield"
+                    variant="filled"
+                    fullWidth label="Enter Poster URL" type="text"
+                    value={values.poster}
+                    onChange={handleChange}
+                    error={touched.poster && errors.poster}
+                    helperText={touched.poster && errors.poster ? errors.poster : ""}
 
-                        onChange={handleChange1} />
-
-
-                    <TextField
-                        key={editMovie.rating}
-                        className="inputfield"
-                        variant="filled"
-                        fullWidth type="text"
-                        name="rating"
-                        defaultValue={editMovie.rating}
-                        onChange={handleChange2} />
+                    onBlur={handleBlur} />
+                {touched.poster && errors.poster ? errors.poster : ""}
 
 
-                    <TextField
-                        key={editMovie.summary}
-                        className="inputfield"
-                        variant="filled"
-                        fullWidth type="text"
-                        name="summary"
-                        defaultValue={editMovie.summary}
-                        onChange={handleChange3} />
-
-                    <TextField
-                        key={editMovie.trailer}
-                        className="inputfield"
-                        variant="filled"
-                        fullWidth type="text"
-                        defaultValue={editMovie.trailer}
-                        name="trailer"
-                        onChange={handleChange4} />
+                <TextField
+                    id="rating"
+                    name="rating"
+                    className="inputfield"
+                    variant="filled"
+                    fullWidth label="Enter Movie Ratings" type="text"
+                    value={values.rating}
+                    onChange={handleChange}
+                    error={touched.rating && errors.rating}
+                    helperText={touched.rating && errors.rating ? errors.rating : ""}
+                    onBlur={handleBlur} />
+                {touched.rating && errors.rating ? errors.rating : ""}
 
 
+                <TextField
+                    id="summary"
+                    name="summary"
+                    className="inputfield"
+                    variant="filled"
+                    fullWidth label="Write Movie Summary" type="text"
+                    value={values.summary}
+                    onChange={handleChange}
+                    error={touched.summary && errors.summary}
+                    helperText={touched.summary && errors.summary ? errors.summary : ""}
+                    onBlur={handleBlur} />
+                {touched.summary && errors.summary ? errors.summary : ""}
 
-                    <Button onClick={UpdateMovie} variant="contained" disableElevation>
-                        Update
-                    </Button>
-                    <Button onClick={() => history.push("../Mymovies")} variant="contained" disableElevation>
-                        Cancel
-                    </Button>
+                <TextField
+                    id="trailer"
+                    name="trailer"
+                    className="inputfield"
+                    variant="filled"
+                    fullWidth label="trailer URL" type="text"
+                    value={values.trailer}
+                    onChange={handleChange}
+                    error={touched.trailer && errors.trailer}
+                    helperText={touched.trailer && errors.trailer ? errors.trailer : ""}
+                    onBlur={handleBlur} />
+                {touched.trailer && errors.trailer ? errors.trailer : ""}
+                {/* 
 
-                    {/* <MovieList movies={movieList} setMovieList={setMovieList} /> */}
-                </div>}
-            </>
+                <Fab color="primary" aria-label="add"
+                    onClick={addMovie}>
+                    <AddIcon />
+                </Fab> */}
+
+                <Button type="submit" variant="contained" disableElevation>
+                    Update
+                </Button>
+                <Button onClick={() => history.push("/Mymovies")} variant="contained" disableElevation>
+                    Back to Movies
+                </Button>
+
+
+
+                {/* <MovieList movies={movieList} setMovieList={setMovieList} /> */}
+            </form>}
+
+
         </Box>
+
+
     );
 }
+
+
+//CRUD completed now you can build any app in world
